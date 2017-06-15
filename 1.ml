19f36fb434c8c994b17f56c81e5e1a13cc11f0f9
Hikaru Suido <hikaru.suido@gmail.com>
2017-06-16 02:46:02 +0900

    Add some methods

--- 1.ml
+++ 1.ml
@@ -1,3 +1,23 @@
-type stack =
-        |Nil
-        |Cell of 'a * 'a stack
+module List = struct
+
+exception Empty
+
+type 'a list =
+        | Nil
+        | Cell of 'a * 'a list
+
+let create =
+        Nil
+;;
+
+let unshift ele lis =
+        Cell (ele, lis)
+;;
+
+let shift lis =
+        match lis with
+          | Nil -> Nil
+          | Cell (_, res) -> res
+;;
+
+end;;
